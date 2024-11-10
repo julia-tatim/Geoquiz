dbcd3a02531f9fb0d0a45c92b3e0dafd214732a3
EricBinek <ericbineksant@gmail.com>
2024-11-10 19:10:45 -0300

    Bandeiras

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -106,6 +107,7 @@ class MainActivity : ComponentActivity() {
                     .fillMaxWidth()
                     .align(Alignment.CenterHorizontally)
                     .padding(horizontal = 8.dp, vertical = 10.dp)
+                    .clip(RoundedCornerShape(25.dp))
             )
 
             Spacer(modifier = Modifier.height(20.dp))
